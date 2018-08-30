c823534707d0fa9eae0fc0dd7a403b45fa8cf05e
JohnHero13 <40628442+JohnHero13@users.noreply.github.com>
2018-08-30 13:10:14 +0200

    Update exercise13_1.py

--- exercise13_1.py
+++ exercise13_1.py
@@ -3,7 +3,7 @@ import ssl
 import xml.etree.ElementTree as ET
 
 
-# Ignore SSL certificate errors
+## Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
